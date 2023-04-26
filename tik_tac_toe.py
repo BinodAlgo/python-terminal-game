23155683c80a531e06d4a1826b7acfa311cd4a95
BinodAlgo <achabinod1112@gmail.com>
2023-04-26 15:09:15 +0000

    add game start message

--- tik_tac_toe.py
+++ tik_tac_toe.py
@@ -1,2 +1,8 @@
 # Python Terminal Game - Tik Tac Toe 
 game_name = "Python"
+print("\n", "-"*(18+len(game_name)), "\n")
+print("*"*9, "Tik-Tac-Toe", "*"*9)
+print("\n", "-"*(18+len(game_name)), "\n")
+# Welcome players
+print("Welcome Players, Game Started !!")
+
