1ed6efce4589eddae2cc703e63ca2218d898865d
BinodAlgo <achabinod1112@gmail.com>
2023-04-26 15:10:46 +0000

    initialize game board

--- tik_tac_toe.py
+++ tik_tac_toe.py
@@ -6,3 +6,9 @@ print("\n", "-"*(18+len(game_name)), "\n")
 # Welcome players
 print("Welcome Players, Game Started !!")
 
+# Initialize game board where players can play with eachother
+def initialize_board():
+  # Initiaze a game board of 3x3 matrix
+  board = [[" " for i in range(3)] for j in range(3)]
+  return board
+
